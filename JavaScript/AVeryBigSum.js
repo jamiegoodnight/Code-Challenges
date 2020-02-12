5ec3fea9be65b5e7d2b75c475dc53475caca1cbd
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-02-11 23:41:33 -0500

    :sparkles: A Very Big Sum

---
+++ JavaScript/AVeryBigSum.js
@@ -0,0 +1,9 @@
+function aVeryBigSum(ar) {
+    let sum=ar.reduce((total, ele) => {
+      return total + ele
+    }, 0)
+    return sum
+    }
+    
+    aVeryBigSum([12343234324,234324234234,234234234,34234234,34234234])
+    
