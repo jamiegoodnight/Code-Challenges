f587ab4db32e690dae3d37c514262897ef64c84e
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-04-03 22:54:25 -0400

    :pencil: Change and Count10 explainer text

--- JavaScript/Count10More.js
+++ JavaScript/Count10More.js
@@ -11,3 +11,6 @@ const count = (start) => {
   return newPhrase
 }
 
+// Count takes in a number and returns that number plus 10 in a
+// series beginning with that number followed by "then", e.g, 1 then 2
+// then 3 then 4 then 5 then 6 then 7 then 8 then 9 then 10"
